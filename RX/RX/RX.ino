300f008a6180e4b614f8e842ba0e9bd346343153
kntzn <kntznart@gmail.com>
2019-08-06 00:14:48 +0300

    [Init]: initial commit

---
+++ RX/RX/RX.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		RX.ino
+ Created:	8/6/2019 12:13:15 AM
+ Author:	kntzn
+*/
+
+void initialize ()
+    { 
+    // inits the uC
+    init ();
+
+    }
+
+int main ()
+    { 
+    initialize ();
+    }
