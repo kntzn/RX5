08f4997bc513c0c422e02d698963f0abf66599ae
kntzn <kntznart@gmail.com>
2019-08-06 00:58:44 +0300

    [Add]: pinout

--- RX/RX/RX.ino
+++ RX/RX/RX.ino
@@ -4,11 +4,43 @@
  Author:	kntzn
 */
 
+#include "Pinout.h"
+
 void initialize ()
     { 
     // inits the uC
     init ();
 
+    pinMode (HC12_SET,      OUTPUT);
+    digitalWrite (HC12_SET, HIGH);
+
+    pinMode (HALL,          INPUT);
+    pinMode (PPM,           OUTPUT);
+    
+    // Lights
+    pinMode (LIGHTS_FRONT,  OUTPUT);
+    analogWrite (LIGHTS_FRONT,   0);
+    pinMode (LIGHTS_BACK,   OUTPUT);
+    analogWrite (LIGHTS_BACK,    0);
+    
+    pinMode (BUZZER,        OUTPUT);
+    
+    // Unused pins
+    pinMode (GROUNDED_0,    INPUT);
+    pinMode (GROUNDED_1,    INPUT);
+
+    pinMode (SD_CS,         OUTPUT);
+
+    // XTL Wattmeter
+    pinMode (SHUNT_POS,     INPUT);
+    pinMode (SHUNT_NEG,     INPUT);
+    pinMode (V_BAT,         INPUT);
+
+    pinMode (THERM,         INPUT);
+
+    // Display 
+    pinMode (DISPLAY_SDA,  OUTPUT);
+    pinMode (DISPLAY_SCL,  OUTPUT);
     }
 
 int main ()
